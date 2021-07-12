51248d6803c2a073ef8dac53b32fcba63ed6cf5e
thanh thien <thanhthien3046@gmail.com>
2021-07-12 15:34:35 +0900

    second commit

---
+++ database/migrations/2021_07_10_102256_create_sub_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 150);
+            $table->string('background')->nullable();
+            $table->bigInteger('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_category');
+    }
+}
---
+++ database/migrations/2021_07_10_102309_create_course.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 150);
+            $table->string('background')->nullable();
+            $table->bigInteger('subcategory_id');
+            $table->string('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course');
+    }
+}
